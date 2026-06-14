64ed9ff8ace6f85a9c7405c9c5ab129df11b01fe
Mr-Hoshom <173585232+Ahmed-Hishaam@users.noreply.github.com>
2026-06-14 21:22:22 +0300

    shiny fix

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -15,7 +15,6 @@ router.get("/", usrctlr.getUsers);
 router.post("/", usrctlr.createUser);
 
 router.get("/:id", usrctlr.getUser);
-router.put("/:id", usrctlr.updateUser);
 router.put("/updateMe", usrctlr.updateMe);
 router.delete("/:id", usrctlr.deleteUser);
 
